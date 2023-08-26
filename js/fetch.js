4635b25651db8665a0eebf9a12e43666cbd103c3
Sushil97 <sushil500530@gmail.com>
2023-08-26 19:41:17 +0600

    sample fetch api

---
+++ js/fetch.js
@@ -0,0 +1,17 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => console.log(response)) // .json is not similer but close to JSON.parse
+    //   .then(json => console.log(json))
+
+
+// structure:
+// const url = 'https://jsonplaceholder.typicode.com/todos/1' ;
+// fetch(url) 
+//     .then(promise => promise.json()) 
+//     .then(get => console.log(get))
+
+function loadData(){
+const url = 'https://jsonplaceholder.typicode.com/todos/1' ;
+fetch(url) 
+    .then(promise => promise.json()) 
+    .then(get => console.log(get))
+}
